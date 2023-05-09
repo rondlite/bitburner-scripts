b95a99dcf8a52928c1b6b9461a3060df2f446ad2
Ron W. van Etten <rondlite@gmail.com>
2023-05-09 07:48:31 +0200

    initial

--- git-pull.js
+++ git-pull.js
@@ -1,6 +1,6 @@
 let options;
 const argsSchema = [
-    ['github', 'alainbryden'],
+    ['github', 'rondlite'],
     ['repository', 'bitburner-scripts'],
     ['branch', 'main'],
     ['download', []], // By default, all supported files in the repository will be downloaded. Override with just a subset of files here
@@ -38,8 +38,7 @@ export async function main(ns) {
         else
             ns.tprint(`WARNING: "${localFilePath}" was not updated. (Currently running or not located at ${remoteFilePath} )`)
     }
-    ns.tprint(`INFO: Pull complete. If you have any questions or issues, head over to the Bitburner #alains-scripts Discord channel: ` +
-        `https://discord.com/channels/415207508303544321/935667531111342200`);
+    ns.tprint(`INFO: Pull complete.`);
     // Remove any temp files / scripts from the prior version
     ns.run(`${options.subfolder}/cleanup.js`);
 }
